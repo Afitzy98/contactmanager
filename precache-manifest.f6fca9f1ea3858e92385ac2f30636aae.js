227db87c19cacb6ff830542b73f9ce7d84a148cc
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2019-01-22 22:15:17 +0000

    Updates

---
+++ precache-manifest.f6fca9f1ea3858e92385ac2f30636aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9e6804e62e1131894cde",
+    "url": "/contactmanager/static/js/main.9e6804e6.chunk.js"
+  },
+  {
+    "revision": "bc1d209ba00fcfb2de7f",
+    "url": "/contactmanager/static/js/1.bc1d209b.chunk.js"
+  },
+  {
+    "revision": "9e6804e62e1131894cde",
+    "url": "/contactmanager/static/css/main.b7eb972e.chunk.css"
+  },
+  {
+    "revision": "bc1d209ba00fcfb2de7f",
+    "url": "/contactmanager/static/css/1.aa455303.chunk.css"
+  },
+  {
+    "revision": "e2e35f8b1d36d645987060d68049096d",
+    "url": "/contactmanager/index.html"
+  }
+];
